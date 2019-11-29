4a6fe2e2b5543b097a9350b31be1ef39945c8ea8
Engkit Satia Riswara <wb-engkitsat462664@antfin.com>
2019-11-29 18:56:35 +0700

    Initial Commit

---
+++ NostraNewsAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NostraNewsAPI
+//
+//  Created by Engkit Satia Riswara on 29/11/19.
+//  Copyright © 2019 Engkit Satia Riswara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
